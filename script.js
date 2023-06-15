2098a2117dc78b5248960817891d131da3209bc2
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-06-15 18:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
   
-  
+  let table=document.getElementById('sampleTable');
+
+let tr=document.createElement('tr');
+let tdLeft=document.createElement('td');
+let tdRight=document.createElement('td');
+
+tdLeft.innerText="New Cell1";
+tdRight.innerText="New Cell2";
+
+tr.append(tdLeft);
+tr.append(tdRight);
+
+table.insertBefore(tr,table.children[0]);
 }
